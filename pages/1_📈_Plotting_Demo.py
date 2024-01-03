0c6bc54c9798d070cb85e46a47c7cb06662af798
Joseph <joeoani@gmail.com>
2024-01-03 11:32:48 -0500

    Initial file

--- pages/1_📈_Plotting_Demo.py
+++ pages/1_📈_Plotting_Demo.py
@@ -8,8 +8,8 @@ st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
 st.write(
     """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
+Streamlit. Generating a bunch of random numbers in a loop for around of 
+5 seconds!"""
 )
 
 progress_bar = st.sidebar.progress(0)
--- pages/3_📊_DataFrame_Demo.py
+++ pages/3_📊_DataFrame_Demo.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
 st.markdown("# DataFrame Demo")
 st.sidebar.header("DataFrame Demo")
 st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
+    """This demo shows a visualization of UN data in Pandas DataFrames.
 (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
 )
 
